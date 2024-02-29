114b4c5b8f377a17a4d961f06d7e6e0eef3fcaaa
kohei <kohei.kubota002@gmail.com>
2024-03-01 01:07:48 +0900

    3つのランダム生成したListを返却するメソッドを実装

--- assignment/dice.cs
+++ assignment/dice.cs
@@ -8,5 +8,22 @@ namespace assignment
 {
     internal class dice
     {
+        //クラスを生成した際、三つのランダムの値を生成する
+        public List<int> Dice()
+        {
+            List<int> diceList = new List<int>(); //返却用
+            //三つのランダムの数値を生成　サイコロなので1-6の範囲
+            Random rand = new Random();
+            int dice1 = rand.Next(1, 7);
+            int dice2 = rand.Next(1, 7);
+            int dice3 = rand.Next(1, 7);
+
+            //Listに格納
+            diceList.Add(dice1);
+            diceList.Add(dice2);
+            diceList.Add(dice3);
+            
+            return diceList;
+        }
     }
 }
